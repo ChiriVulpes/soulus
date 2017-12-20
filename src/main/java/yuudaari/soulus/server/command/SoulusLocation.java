023b5df9b39c8b099a50ad52552cecd36213eb08
Mackenzie McClane <yuudaari@gmail.com>
2017-12-19 22:17:26 -0600

    Fix souluslocation command
    idk why this worked fine for me

--- src/main/java/yuudaari/soulus/server/command/SoulusLocation.java
+++ src/main/java/yuudaari/soulus/server/command/SoulusLocation.java
@@ -1,7 +1,5 @@
 package yuudaari.soulus.server.command;
 
-import com.mojang.realmsclient.gui.ChatFormatting;
-
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
@@ -9,6 +7,7 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 
 public class SoulusLocation extends CommandBase {
@@ -28,8 +27,8 @@ public class SoulusLocation extends CommandBase {
 		World world = ics.getEntityWorld();
 		ResourceLocation biome = world.getBiome(pos).getRegistryName();
 
-		ics.sendMessage(new TextComponentString("Dimension: " + ChatFormatting.GOLD
-				+ world.provider.getDimensionType().getName() + ChatFormatting.WHITE + ", Biome: " + ChatFormatting.RED
-				+ biome.getResourceDomain() + ":" + ChatFormatting.GOLD + biome.getResourcePath()));
+		ics.sendMessage(new TextComponentString("Dimension: " + TextFormatting.GOLD
+				+ world.provider.getDimensionType().getName() + TextFormatting.WHITE + ", Biome: " + TextFormatting.RED
+				+ biome.getResourceDomain() + ":" + TextFormatting.GOLD + biome.getResourcePath()));
 	}
 }
