6191c08cef2e17789c4f8592d2db85d498caf82f
Mackenzie McClane <yuudaari@gmail.com>
2018-12-31 15:28:15 -0600

    Fix creeper explosion handler assuming that all exploders are creepers

--- src/main/java/yuudaari/soulus/common/misc/CreeperExplosion.java
+++ src/main/java/yuudaari/soulus/common/misc/CreeperExplosion.java
@@ -1,7 +1,8 @@
 package yuudaari.soulus.common.misc;
 
 import java.util.stream.Collectors;
-import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.monster.EntityCreeper;
 import net.minecraft.potion.PotionEffect;
 import net.minecraftforge.event.world.ExplosionEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -12,11 +13,14 @@ public class CreeperExplosion {
 
 	@SubscribeEvent
 	public static void onExplode (ExplosionEvent event) {
-		EntityLivingBase exploder = (EntityLivingBase) event.getExplosion().exploder;
+		Entity exploder = event.getExplosion().exploder;
+		if (!(exploder instanceof EntityCreeper)) return;
 
-		for (PotionEffect potionEffect : exploder.getActivePotionEffects().stream().collect(Collectors.toList())) {
+		EntityCreeper creeper = (EntityCreeper) exploder;
+
+		for (PotionEffect potionEffect : creeper.getActivePotionEffects().stream().collect(Collectors.toList())) {
 			if (potionEffect.getDuration() > Short.MAX_VALUE) {
-				exploder.removePotionEffect(potionEffect.getPotion());
+				creeper.removePotionEffect(potionEffect.getPotion());
 			}
 		}
 	}
