7a3c1feecf27706ac1db120313a3ece401eb5c12
Mackenzie McClane <yuudaari@gmail.com>
2019-11-02 09:31:17 -0500

    Server debug profile

--- debug.js
+++ debug.js
@@ -1,6 +1,6 @@
 const exec = require("child_process").exec;
 
-exec("@echo off & start gradlew runClient --debug-jvm & timeout 20 & exit");
+exec(`@echo off & start gradlew ${process.argv.includes("-server") ? "runServer" : "runClient"} --debug-jvm & timeout 20 & exit`);
 
 setTimeout(() => {
 	process.exit();
